34983d319f4c2a9c114072bcc964330f0e55e962
namewasntfound <namewasntfound@gmail.com>
2022-07-30 02:15:32 +0300

    Updated DashboardController with changes to patients and doctors view.

--- src/main/java/com/hospitalmanagement/controllers/DashboardController.java
+++ src/main/java/com/hospitalmanagement/controllers/DashboardController.java
@@ -1,14 +1,28 @@
 package com.hospitalmanagement.controllers;
 
 
+import com.hospitalmanagement.appuser.AppUserService;
+import com.hospitalmanagement.patientmanagement.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
+    private final AppUserService appUserService;
+    private final PatientService patientService;
+
+    @Autowired
+    public DashboardController(AppUserService appUserService, PatientService patientService) {
+        this.appUserService = appUserService;
+        this.patientService = patientService;
+    }
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("doctors", appUserService.getDoctors());
+        model.addAttribute("patients", patientService.getPatients());
         return "index";
     }
 }
